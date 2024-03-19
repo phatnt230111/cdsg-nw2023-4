e218b502e45ea7e03df34544b9a976a60a208957
phatnt230111 <163012472+phatnt230111@users.noreply.github.com>
2024-03-18 22:03:02 -0400

    created_shared.sh

--- created_shared.sh
+++ created_shared.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ -d "/home/nw-4" ]; then
-        echo "folder nw-4 has been created"
-else
-        mkdir /home/nw-4
-if [ -d "/home/shared" ]; then
-        echo "folder shared has been created"
-else
-    mkdir -p /home/shared
-fi
-
+# if [ -d "/home/nw-4" ]; then
+#        echo "folder nw-4 has been created"
+# else
+#        mkdir /home/nw-4
+# if [ -d "/home/shared" ]; then
+#        echo "folder shared has been created"
+# else
+#    mkdir -p /home/shared
+# fi
+mkdir /home/nw-4
+mkdir /home/shared
 chown -R NT_Phat:nw-4 /home/nw-4
 chown -R NT_Phat:cdsg /home/shared
 chmod -R 770 /home/nw-4
